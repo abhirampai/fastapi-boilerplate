52d264020db11c0b64e8c6e8cda1cfcf5ed26ab0
Pai026 <abhirampai1999@gmail.com>
2021-03-25 15:12:20 +0530

    checking login and register capabilities

---
+++ src/main.py
@@ -0,0 +1,42 @@
+from typing import Optional
+from .auth import AuthHandler
+from .schemas import AuthDetails
+
+from fastapi import FastAPI, Depends, HTTPException
+
+app = FastAPI()
+
+auth_handler = AuthHandler()
+users = []
+
+@app.post("/register", status_code=201)
+def register(auth_details: AuthDetails):
+    if any(x['username'] == auth_details.username for x in users):
+        raise HTTPException(status=400, detail='Username is already present')
+    hashed_password = auth_handler.get_password_hash(auth_details.password)
+    users.append({
+        'username': auth_details.username,
+        'password': hashed_password
+    })
+    return 
+
+@app.post('/login')
+def login(auth_details: AuthDetails):
+    user= None
+    for x in users:
+        if x['username'] == auth_details.username:
+            user = x
+            break
+    if (user is None) or (not auth_handler.verify_password(auth_details.password, user['password'])):
+        raise HTTPException(status_code=401, detail='Invalid username and/or password')
+    
+    token = auth_handler.encode_token(user['username'])
+    return {'token': token}
+
+@app.get('/unprotected')
+def unprotected():
+    return {'hello': 'world'}
+
+@app.get('/protected')
+def protected(username=Depends(auth_handler.auth_wrapper)):
+    return {'name':username}
