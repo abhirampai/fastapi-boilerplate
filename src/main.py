f15e2d9997d44ebadecb0ce9810bea53a489080d
Pai026 <abhirampai1999@gmail.com>
2021-03-25 16:02:58 +0530

    added mongodb connection

--- src/main.py
+++ src/main.py
@@ -1,20 +1,28 @@
 from typing import Optional
 from .auth import AuthHandler
 from .schemas import AuthDetails
-
+import pymongo
+from pymongo import MongoClient
 from fastapi import FastAPI, Depends, HTTPException
+from dotenv import dotenv_values,find_dotenv
+
 
 app = FastAPI()
 
 auth_handler = AuthHandler()
-users = []
+config=dotenv_values(find_dotenv())
+cluster = MongoClient(config["MONGO_URL"])
+db = cluster[config["DATABASE"]]
+collection = db[config["USERS"]]
 
 @app.post("/register", status_code=201)
 def register(auth_details: AuthDetails):
-    if any(x['username'] == auth_details.username for x in users):
-        raise HTTPException(status=400, detail='Username is already present')
+    print(collection.find_one({"username":auth_details.username}))
+    if collection.find_one({"username":auth_details.username}):
+        raise HTTPException(status_code=400, detail='Username is already present')
     hashed_password = auth_handler.get_password_hash(auth_details.password)
-    users.append({
+    collection.insert_one(
+        {
         'username': auth_details.username,
         'password': hashed_password
     })
@@ -23,10 +31,9 @@ def register(auth_details: AuthDetails):
 @app.post('/login')
 def login(auth_details: AuthDetails):
     user= None
-    for x in users:
-        if x['username'] == auth_details.username:
-            user = x
-            break
+    getUser=collection.find_one({"username":auth_details.username})
+    if getUser:
+        user = getUser
     if (user is None) or (not auth_handler.verify_password(auth_details.password, user['password'])):
         raise HTTPException(status_code=401, detail='Invalid username and/or password')
     
